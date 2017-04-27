e19a6de60ee28ee512783e3bfe08b8e0e23c4b9c
Laura-O <laura.ohrndorf@gmail.com>
2017-04-27 23:41:28 +0200

    Initial version and README * Added an initial version * Started with the Readme

--- Project_6/README.md
+++ Project_6/README.md
@@ -1,9 +1,38 @@
 # Project 6
 
 ## Summary
+This visualization is based on the database export of the [World Cube Association](http://www.worldcubeassociation.org)(WCA). The WCA governs competitions for twisted puzzles, with the widely know "Rubik's Cube" as most popular event. In this project, I will create a visualization for the number of competitions individual competitors have attended. I recently attended my 100th competition, which makes me the 25th person ever to reach this benchmark.
 
 ## Design
 
+
 ## Feedback
+### Boring design
+The first feedback for the initial design was that the black/white design was rather boring and not very appealing.
+
+I decided to use additional information to colour the bubbles. Despite the name and the number of comeptitions, the dataset also includes the nationalities. Therefore, I used a category scale to colour each bubble according to the nationality the competitor has.
+
+### Bubble are difficult to differentiate and too small
+Another point was that the bubbles are difficult to differentiate and too small. This is partly solved by the improvement mentioned in the first step. The size was still a problem though. I decided to upscale the complete visualization and set the range of the bubbles to 10-50 instead of 2-20.
+
+### Static
+This feedback was a very basic one: the reviewer said that he does not see any advantage in using a web application for this kind of visualization.
+
+This feedback resulted in two changes:
+#### Use of forces
+d3-force is a neat module to simulate physical forces. There are several forces which can be used. I chose *n-body* and *collide*.
+
+*n-body* applies mutually amongst all elements. By setting a positive value for strength it simulates gravity. By setting a negative value it's electrostatic charge. I decided to use a positive strength of 10.
+
+Additionally, I used the collide function, which creates a circle collision with a specified radius. I set the radius to the radius of the node + 20.
+
+
+#### Drag & drop function
+
 
 ## Resources
+* [D3 Wiki and documentation](https://github.com/d3/d3/wiki)
+* [Udacity Website](http://www.udacity.com)
+* [d3-force](https://github.com/d3/d3-force)
+* [D3.js version 4](https://anthonyskelton.com/2016/d3-js-version-4/)
+* [Bubble charts in D3](https://jrue.github.io/coding/2014/exercises/basicbubblepackchart/)
--- Project_6/initial/js/bubble_chart.js
+++ Project_6/initial/js/bubble_chart.js
@@ -1,7 +1,6 @@
 function bubbleChart() {
-    var width = 960,
-        height = 960,
-        columnForColors = "countryId",
+    var width = 480,
+        height = 480,
         ncomps = "value";
 
     function chart(selection) {
@@ -10,30 +9,26 @@ function bubbleChart() {
             svg = div.selectAll('svg');
         svg.attr('width', width).attr('height', height);
 
-        var colour = d3.scaleOrdinal(d3.schemeCategory20b);
-
         var tooltip = selection
             .append("div")
-            .attr("class","tooltip")
-
+            .style("position", "absolute")
+            .style("visibility", "hidden")
+            .style("color", "white")
+            .style("padding", "4px")
+            .style("background-color", "black")
+            .style("text-align", "center")
             .text("");
 
         var simulation = d3.forceSimulation(data)
             .force("x", d3.forceX(480).strength(0.05))
             .force("y", d3.forceY(480).strength(0.05))
-            .force("charge", d3.forceManyBody().strength(10))
-            .force("collide",
-                d3.forceCollide()
-                .radius(function(d) {
-                    return d.r + 20;
-                })
-                .iterations(2));
+            .force("charge", d3.forceManyBody(100));
 
         var scaleRadius = d3.scaleLinear().domain([d3.min(data, function(d) {
             return +d[ncomps];
         }), d3.max(data, function(d) {
             return +d[ncomps];
-        })]).range([10, 50])
+        })]).range([2, 20])
 
         var node = svg.selectAll(".circles")
             .data(data)
@@ -42,9 +37,8 @@ function bubbleChart() {
             .attr('r', function(d) {
                 return scaleRadius(d[ncomps])
             })
-            .attr("fill", (d, i) => colour(d[columnForColors]))
             .on("mouseover", function(d) {
-                tooltip.html(d[columnForColors] + "<br>" + d.name + "<br>" + d[ncomps] + " competitions");
+                tooltip.html(d.name + "<br>" + d[ncomps] + " competitions");
                 return tooltip.style("visibility", "visible");
             })
             .on("mousemove", function() {
@@ -52,49 +46,18 @@ function bubbleChart() {
             })
             .on("mouseout", function() {
                 return tooltip.style("visibility", "hidden");
-            })
-            .call(d3.drag()
-                .subject(dragsubject)
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended));
-
-        d3.selectAll("circle")
-            .data()
-            .forEach(d => d.r = Math.random() * 15);
-
-        function dragsubject() {
-            return simulation.find(d3.event.x, d3.event.y, d3.event.r);
-        }
-
-        function dragstarted() {
-            if (!d3.event.active) simulation.alphaTarget(0.02).restart();
-            d3.event.subject.fx = d3.event.subject.x;
-            d3.event.subject.fy = d3.event.subject.y;
-        }
-
-        function dragged() {
-            d3.event.subject.fx = d3.event.x;
-            d3.event.subject.fy = d3.event.y;
-        }
-
-        function dragended() {
-            if (!d3.event.active) simulation.alphaTarget(0);
-            d3.event.subject.fx = null;
-            d3.event.subject.fy = null;
-        }
+            });
 
         node.transition().duration(400).attr('r', function(d) {
             return scaleRadius(d[ncomps])
         })
-
-        //    simulation.alpha(0.8).restart();
 
         simulation.nodes(data)
             .on("tick", d => {
                 node.attr("cx", d => d.x).attr("cy", d => d.y);
             });
     }
+
     chart.width = function(value) {
         if (!arguments.length) {
             return width;
@@ -108,15 +71,6 @@ function bubbleChart() {
             return height;
         }
         height = value;
-        return chart;
-    };
-
-
-    chart.columnForColors = function(value) {
-        if (!arguments.columnForColors) {
-            return columnForColors;
-        }
-        columnForColors = value;
         return chart;
     };
 
