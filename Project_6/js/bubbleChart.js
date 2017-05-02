75b27bb562c61fcccfcc74b5ac6fbe5b8cc11a11
Laura-O <laura.ohrndorf@gmail.com>
2017-05-02 18:52:24 +0200

    Improve project * Filled sections in Readme * Added comments to code * Some minor refinements

---
+++ Project_6/js/bubbleChart.js
@@ -0,0 +1,142 @@
+function bubbleChart() {
+    var width = 960,
+        height = 960,
+        columnForColors = "countryId",
+        ncomps = "value";
+
+    // Create the chart
+    function chart(selection) {
+        var data = selection.enter().data();
+        var div = selection,
+            svg = div.selectAll("svg");
+
+        svg.attr("width", width).attr("height", height);
+
+        // Set color by country
+        var colour = d3.scaleOrdinal(d3.schemeCategory20);
+
+        var tooltip = selection
+            .append("div")
+            .attr("class", "tooltip")
+            .text("");
+
+        // Create new simulation with forces
+        var simulation = d3.forceSimulation(data)
+            .force("x", d3.forceX(480).strength(0.05))
+            .force("y", d3.forceY(480).strength(0.05))
+            .force("charge", d3.forceManyBody().strength(10))
+            .force("collide",
+                d3.forceCollide()
+                .radius(function(d) {
+                    return d.r + 20;
+                })
+                .iterations(2));
+
+        // Set the radius of the bubbles according to ncomps
+        var scaleRadius = d3.scaleLinear().domain([d3.min(data, function(d) {
+            return +d[ncomps];
+        }), d3.max(data, function(d) {
+            return +d[ncomps];
+        })]).range([10, 50])
+
+        // Draw circles
+        var node = svg.selectAll(".circles")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("r", function(d) {
+                return scaleRadius(d[ncomps])
+            })
+            .attr("fill", (d, i) => colour(d[columnForColors]))
+            .attr("fill-opacity", "0.8")
+            // Show tooltip on mouseover
+            .on("mouseover", function(d) {
+                d3.select(this).attr("fill-opacity", "1"); // change opacity to 1
+                tooltip.html(d[columnForColors] + "<br>" +
+                    d.name + "<br>" + d[ncomps] + " competitions");
+                return tooltip.style("visibility", "visible");
+            })
+            // Move tooltip when mouse is moved over circle
+            .on("mousemove", function() {
+                return tooltip.style("top", (d3.event.pageY - 10) + "px")
+                    .style("left", (d3.event.pageX + 10) + "px");
+            })
+            // Remove tooltip
+            .on("mouseout", function() {
+                d3.select(this).attr("fill-opacity", "0.8");
+                return tooltip.style("visibility", "hidden");
+            })
+            // Bind methods to drag events
+            .call(d3.drag()
+                .subject(dragsubject)
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
+
+        d3.selectAll("circle")
+            .data()
+            .forEach(d => d.r = Math.random() * 15);
+
+        function dragsubject() {
+            return simulation.find(d3.event.x, d3.event.y, d3.event.r);
+        }
+
+        function dragstarted() {
+            if (!d3.event.active) simulation.alphaTarget(0.02).restart();
+            d3.event.subject.fx = d3.event.subject.x;
+            d3.event.subject.fy = d3.event.subject.y;
+        }
+
+        function dragged() {
+            d3.event.subject.fx = d3.event.x;
+            d3.event.subject.fy = d3.event.y;
+        }
+
+        function dragended() {
+            if (!d3.event.active) simulation.alphaTarget(0);
+            d3.event.subject.fx = null;
+            d3.event.subject.fy = null;
+        }
+
+        // Listen for tick events to arrange nodes
+        simulation.nodes(data)
+            .on("tick", d => {
+                node.attr("cx", d => d.x).attr("cy", d => d.y);
+            });
+    }
+
+    chart.width = function(value) {
+        if (!arguments.length) {
+            return width;
+        }
+        width = value;
+        return chart;
+    };
+
+    chart.height = function(value) {
+        if (!arguments.length) {
+            return height;
+        }
+        height = value;
+        return chart;
+    };
+
+
+    chart.columnForColors = function(value) {
+        if (!arguments.columnForColors) {
+            return columnForColors;
+        }
+        columnForColors = value;
+        return chart;
+    };
+
+    chart.ncomps = function(value) {
+        if (!arguments.ncomps) {
+            return ncomps;
+        }
+        ncomps = value;
+        return chart;
+    };
+
+    return chart;
+}
